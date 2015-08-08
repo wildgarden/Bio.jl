f1df8dcf9fe829476bf22b2204c61e6c23820796
Kenta Sato <bicycle1885@gmail.com>
2015-08-08 14:16:25 +0900

    check cost value

--- src/align/model.jl
+++ src/align/model.jl
@@ -3,6 +3,7 @@ abstract AlignmentModel
 # cost model is about a minimizing problem
 abstract AbstractCostModel <: AlignmentModel
 
+# gap character: -
 immutable GAP; end
 typealias Character Union{Char,Nucleotide,AminoAcid}
 
@@ -30,10 +31,18 @@ getindex{T}(m::CostModel{T},  ::T,          ::Type{GAP}) = m.char2gap
 getindex{T}(m::CostModel{T},  ::Type{GAP},  ::T        ) = m.gap2char
 getindex{T}(m::CostModel{T}, x::T,         y::T        ) = m.cost[convert(UInt8,x)+1,convert(UInt8,y)+1]
 
-setindex!{T}(m::CostModel{T}, c::Int,  ::Type{GAP}              ) = m.char2gap = m.gap2char = c
-setindex!{T}(m::CostModel{T}, c::Int,  ::T,          ::Type{GAP}) = m.char2gap = c
-setindex!{T}(m::CostModel{T}, c::Int,  ::Type{GAP},  ::T        ) = m.gap2char = c
-setindex!{T}(m::CostModel{T}, c::Int, x::T,         y::T        ) = m.cost[convert(UInt8,x)+1,convert(UInt8,y)+1] = c
+macro check_cost(ex)
+    quote
+        local cost = $(esc(ex))
+        cost ≥ 0 || error("cost should be non-negative (given ", cost, ")")
+        cost
+    end
+end
+
+setindex!{T}(m::CostModel{T}, c::Int,  ::Type{GAP}              ) = m.char2gap = m.gap2char = @check_cost c
+setindex!{T}(m::CostModel{T}, c::Int,  ::T,          ::Type{GAP}) = m.char2gap = @check_cost c
+setindex!{T}(m::CostModel{T}, c::Int,  ::Type{GAP},  ::T        ) = m.gap2char = @check_cost c
+setindex!{T}(m::CostModel{T}, c::Int, x::T,         y::T        ) = m.cost[convert(UInt8,x)+1,convert(UInt8,y)+1] = @check_cost c
 
 
 # score model is a maximizing problem

e0a97e5c6b3c1b9de61609ef3d6540ff88669a61
Kenta Sato <bicycle1885@gmail.com>
2015-08-09 18:05:14 +0900

    reorder score/cost models

--- src/align/model.jl
+++ src/align/model.jl
@@ -1,49 +1,8 @@
 abstract AlignmentModel
 
-# cost model is about a minimizing problem
-abstract AbstractCostModel{T<:Real} <: AlignmentModel
-
 # gap character: -
 immutable GAP; end
 typealias Character Union{Char,Nucleotide,AminoAcid}
-
-# a gap and a mismatch costs 1; otherwise 0
-immutable UnitCostModel{T} <: AbstractCostModel{T}; end
-const UnitCost = UnitCostModel{Int}()
-
-getindex{T}(::UnitCostModel{T},  ::Character,  ::Type{GAP}) = T(1)
-getindex{T}(::UnitCostModel{T},  ::Type{GAP},  ::Character) = T(1)
-getindex{T}(::UnitCostModel{T}, x::Character, y::Character) = ifelse(x === y, T(0), T(1))
-
-minimum_indel_cost{T}(::UnitCostModel{T}) = T(1)
-
-# arbitrary cost model
-type CostModel{C<:Character,T} <: AbstractCostModel{T}
-    cost::Matrix{T}
-    char2gap::T
-    gap2char::T
-    function CostModel(alphabetsize)
-        new(zeros(T, alphabetsize, alphabetsize), 0, 0)
-    end
-end
-
-getindex{C}(m::CostModel{C},  ::C,          ::Type{GAP}) = m.char2gap
-getindex{C}(m::CostModel{C},  ::Type{GAP},  ::C        ) = m.gap2char
-getindex{C}(m::CostModel{C}, x::C,         y::C        ) = m.cost[convert(UInt8,x)+1,convert(UInt8,y)+1]
-
-macro check_cost(ex)
-    quote
-        local cost = $(esc(ex))
-        cost ≥ 0 || error("cost should be non-negative (given ", cost, ")")
-        cost
-    end
-end
-
-setindex!{C}(m::CostModel{C}, c::Real,  ::Type{GAP}              ) = m.char2gap = m.gap2char = @check_cost c
-setindex!{C}(m::CostModel{C}, c::Real,  ::C,          ::Type{GAP}) = m.char2gap = @check_cost c
-setindex!{C}(m::CostModel{C}, c::Real,  ::Type{GAP},  ::C        ) = m.gap2char = @check_cost c
-setindex!{C}(m::CostModel{C}, c::Real, x::C,         y::C        ) = m.cost[convert(UInt8,x)+1,convert(UInt8,y)+1] = @check_cost c
-
 
 # score model is a maximizing problem
 abstract AbstractScoreModel{T<:Real} <: AlignmentModel
@@ -73,4 +32,36 @@ setindex!{C}(m::ScoreModel{C}, s::Real,  ::C,          ::Type{GAP}) = m.char2gap
 setindex!{C}(m::ScoreModel{C}, s::Real,  ::Type{GAP},  ::C        ) = m.gap2char = s
 setindex!{C}(m::ScoreModel{C}, s::Real, x::C,         y::C        ) = m.score[convert(UInt8,x)+1,convert(UInt8,y)+1] = s
 
-# PAM, BLOSUM, etc.
+# TODO: PAM, BLOSUM, etc.
+
+# cost model is about a minimizing problem
+abstract AbstractCostModel{T<:Real} <: AlignmentModel
+
+# a gap and a mismatch costs 1; otherwise 0
+immutable UnitCostModel{T} <: AbstractCostModel{T}; end
+const UnitCost = UnitCostModel{Int}()
+
+getindex{T}(::UnitCostModel{T},  ::Character,  ::Type{GAP}) = T(1)
+getindex{T}(::UnitCostModel{T},  ::Type{GAP},  ::Character) = T(1)
+getindex{T}(::UnitCostModel{T}, x::Character, y::Character) = ifelse(x === y, T(0), T(1))
+
+minimum_indel_cost{T}(::UnitCostModel{T}) = T(1)
+
+# arbitrary cost model
+type CostModel{C<:Character,T} <: AbstractCostModel{T}
+    cost::Matrix{T}
+    char2gap::T
+    gap2char::T
+    function CostModel(alphabetsize)
+        new(zeros(T, alphabetsize, alphabetsize), 0, 0)
+    end
+end
+
+getindex{C}(m::CostModel{C},  ::C,          ::Type{GAP}) = m.char2gap
+getindex{C}(m::CostModel{C},  ::Type{GAP},  ::C        ) = m.gap2char
+getindex{C}(m::CostModel{C}, x::C,         y::C        ) = m.cost[convert(UInt8,x)+1,convert(UInt8,y)+1]
+
+setindex!{C}(m::CostModel{C}, c::Real,  ::Type{GAP}              ) = m.char2gap = m.gap2char = c
+setindex!{C}(m::CostModel{C}, c::Real,  ::C,          ::Type{GAP}) = m.char2gap = c
+setindex!{C}(m::CostModel{C}, c::Real,  ::Type{GAP},  ::C        ) = m.gap2char = c
+setindex!{C}(m::CostModel{C}, c::Real, x::C,         y::C        ) = m.cost[convert(UInt8,x)+1,convert(UInt8,y)+1] = c

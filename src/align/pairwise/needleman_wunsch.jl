e536282f8b9afa14bb9e5dc7d740c3bb8ce2d8cc
Kenta Sato <bicycle1885@gmail.com>
2015-08-10 14:49:23 +0900

    add reference

--- src/align/pairwise/needleman_wunsch.jl
+++ src/align/pairwise/needleman_wunsch.jl
@@ -6,6 +6,10 @@
 # * type: global-global alignment
 # * complexity: O(m*n)
 # * space: O(m*n) for alignment, O(m) for score/distance
+#
+# references:
+# * Needleman, Saul B., and Christian D. Wunsch. "A general method applicable to the search for similarities in the amino acid sequence of two proteins." Journal of molecular biology 48.3 (1970): 443-453.
+# * Sankoff, David. "Matching sequences under deletion/insertion constraints." Proceedings of the National Academy of Sciences 69.1 (1972): 4-6.
 
 
 type NeedlemanWunsch{M<:Union{AbstractScoreModel,AbstractCostModel},T<:Real} <: PairwiseAlignmentAlgorithm
